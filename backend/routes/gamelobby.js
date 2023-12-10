0459bdaa3856a23b833c59565d553b1d4dadfc24
fahimamiri <fahim.amiri21@gmail.com>
2023-12-10 14:33:46 -0800

    db updated

--- backend/routes/gamelobby.js
+++ backend/routes/gamelobby.js
@@ -1,6 +1,6 @@
 const { render, resolveInclude } = require("ejs");
 const express = require("express");
-const { Games } = require("../db")
+const { Games } = require("../db/index")
 const router = express.Router(); 
 
 router.get("/",(request, response)=>{
