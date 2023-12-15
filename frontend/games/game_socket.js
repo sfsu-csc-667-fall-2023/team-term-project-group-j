20e7b9398cf2511809e168b203ebf22f7c553f5a
AsuncionJ43210 <jga0425@gmail.com>
2023-12-14 20:46:42 -0800

    Cleaned up game_socket.js

--- frontend/games/game_socket.js
+++ frontend/games/game_socket.js
@@ -2,6 +2,7 @@ import { io } from "socket.io-client";
 import * as GAME_CONSTANTS from "@constants/games";
 
 console.log("Hello from bundled game socket");
+const userId = document.querySelector("#user-id").value;
 
 let gameSocket;
 
@@ -14,33 +15,43 @@ const configure = (socketId) => {
 
 gameSocket.on(GAME_CONSTANTS.STATE_UPDATED, stateUpdated);
 
-const stateUpdated = ({ blind, pot, communityCards, players }) => {
+const stateUpdated = ({ blind, pot, currentTurn, deck, players }) => {
     // Callback function called when the game state is updated
-    console.log({ blind, pot, communityCards, players });
+    console.log("Game state updated");
+    console.log({ blind, pot, currentTurn, deck, players});
   
     // Update the displayed blind and pot in the UI
     updateBlindAndPot(blind, pot);
+
+    updateCurrentTurn(currentTurn);
   
-    if(communityCards !== -1){
+    if(communityCards !== 0){
         // Update the community cards in the UI
-        updateCommunityCards(communityCards);
+        updateCards(deck);
     }
 
-    for(let x = 0; x < 5; x++){
-        const { email, bank, status} = players[x];
-        if(x == 0){
-            updatePlayerInfo(playerOne, email, bank, status)
-        }else if(x == 1){
-            updatePlayerInfo(playerTwo, email, bank, status)
-        }else if(x == 2){
-            updatePlayerInfo(playerThree, email, bank, status)
-        }else if(x == 3){
-            updatePlayerInfo(playerFour, email, bank, status)
-        }else if(x == 4){
-            updatePlayerInfo(playerFive, email, bank, status)
+    if (Array.isArray(players)) {
+        for(let x = 0; x < players.length; x++){
+            const {user_id, username, bank, folded} = players[x];
+            
+            const isTurn = 0;
+            if(user_id == currentTurn){
+                isTurn = 1;
+            }
+
+            if(x == 0){
+                updatePlayerInfo(playerOne, username, bank, folded, isTurn)
+            } else if(x == 1){
+                updatePlayerInfo(playerTwo, username, bank, folded, isTurn)
+            } else if(x == 2){
+                updatePlayerInfo(playerThree, username, bank, folded, isTurn)
+            } else if(x == 3){
+                updatePlayerInfo(playerFour, username, bank, folded, isTurn)
+            } else if(x == 4){
+                updatePlayerInfo(playerFive, username, bank, folded, isTurn)
+            }
         }
     }
-    
 };
 
 const potElement = document.getElementById(".pot");
@@ -55,13 +66,12 @@ const updateBlindAndPot = (blind, pot) => {
 
     //Remove Check or Call button depending on the pot
     if(pot < 1){
-        //Check
+        //Remove the call button, add check
 
     }else{
-        //Call
+        //Remove the check button, add call
 
     }
-
 };
 
 const ccOne = document.getElementById(".comm-card-one");
@@ -70,9 +80,26 @@ const ccThree = document.getElementById(".comm-card-three");
 const ccFour = document.getElementById(".comm-card-four");
 const ccFive = document.getElementById(".comm-card-five");
 
-// Update the displayed community cards in the UI
-const updateCommunityCards = (communityCards) => {
-    console.log("Community Cards: " + communityCards);
+// Update the displayed cards in the UI
+const updateCards = (deck) => {
+    console.log("Deck " + deck);
+    
+    for(let x = 0; x < deck.length; x++){
+        //Display the community cards
+        if(deck[x].user_id < 0){
+            //If the user_id is -4 or -5, then they should not be revealed yet
+            if(deck[x].user_id == -4){
+
+            }
+            else if (deck[x].user_id == -5){
+
+            }
+
+        }
+        else if(deck[x].user_id == userId){
+            //Display the player's cards
+        }
+    }
 };
 
 const playerOne = document.getElementById(".player-one");
@@ -82,16 +109,18 @@ const playerFour = document.getElementById(".player-four");
 const playerFive = document.getElementById(".player-five");
 
 // Update the displayed information for each player in the UI
-const updatePlayerInfo = (container, email, bank, status) => {
-    if(email !== -1){
-        container.innerHTML = email + ": $" + bank;
+const updatePlayerInfo = (container, username, bank, folded, currentTurn) => {
+    if(username !== -1){
+        container.innerHTML = username + ": $" + bank;
 
          // Change background color based on status
-         if (status === 2) {
-            container.style.backgroundColor = "green";
-        } else if (status === 1) {
+        if (folded === 1) {
             container.style.backgroundColor = "red";
-        } else if (status === 0) {
+        } 
+        else if (currentTurn === 1) {
+            container.style.backgroundColor = "black";
+        }
+        else if (folded === 0) {
             container.style.backgroundColor = "black";
         }
     }else{
