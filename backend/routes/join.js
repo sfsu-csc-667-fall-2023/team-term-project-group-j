accc10c50ee1c0528046737a631bc7d040d821e3
fahimamiri <fahim.amiri21@gmail.com>
2023-12-14 22:39:35 -0800

    removed join.js

--- backend/routes/join.js
+++
@@ -1,8 +0,0 @@
-// Register the handler for the "/join" route
-router.post("/join", handler);
-
-// Register the handler for the "/:id/join" route
-router.post("/:id/join", handler);
-
-// Export the router for use in other parts of the application
-module.exports = router;
